5b8db62af0cf2d4eb6e52a81ccacdde019f78cb1
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2019-11-24 21:10:17 -0700

    Initial Code Base

---
+++ src/Scripts.php
@@ -0,0 +1,12 @@
+<?php
+namespace Wpx\Scripts\v0;
+
+require_once __DIR__ . '/bootstrap.php';
+
+if ( ! class_exists( __NAMESPACE__ . '\Scripts' ) ) {
+
+	class Scripts {
+
+	} // eo class Scripts
+
+} // eo if ( class_exists )
