340d785c5bd60a4d24acc1d8467b866671542608
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2019-11-30 13:34:01 -0700

    Prefix with V to Better Match Namespace

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 namespace Wpx\Scripts\v0;
 
-if ( ! defined( 'WPX_SCRIPTS_0__VERSION' ) ) {
-	define( 'WPX_SCRIPTS_0__VERSION', '0.0.0-alpha.6' );
+if ( ! defined( 'WPX_SCRIPTS_V0__VERSION' ) ) {
+	define( 'WPX_SCRIPTS_V0__VERSION', '0.0.0-alpha.6' );
 }
